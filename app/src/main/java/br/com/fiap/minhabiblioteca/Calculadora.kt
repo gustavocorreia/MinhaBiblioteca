e8902a87c10928066e4a6ae82418e22a6eea20cf
Logon Aluno <logonrmlocal@fiap.com.br>
2019-04-03 21:02:09 -0300

    Initial commit

---
+++ app/src/main/java/br/com/fiap/minhabiblioteca/Calculadora.kt
@@ -0,0 +1,11 @@
+package br.com.fiap.minhabiblioteca
+
+class Calculadora {
+    fun somar(parcela1: Int, parcela2: Int) : Int {
+        return  parcela1 + parcela2
+    }
+
+    fun subtrair(minuendo: Int, subtraendo: Int) : Int {
+        return  minuendo - subtraendo
+    }
+}
